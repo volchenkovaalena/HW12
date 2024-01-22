86b96ea85c489590f991c5418a2fa65e1ca8b330
alena <misamistake@>
2024-01-22 23:51:05 +0300

    Did my HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main ( String[] args ) {
-        System.out.println ( "Hello world!" );
-    }
+public class Main
+{ public static void main ( String[] args ) {
+    Author pushkin = new Author ("Александр", "Пушкин");
+    Author turgenev = new Author ( "Иван","Тургенев" );
+    Book fatharsAndSons = new Book ("Отцы и дети", 1861, turgenev);
+    Book capitansDaughter = new Book ( "Капитанская дочка", 1836, pushkin );
+    System.out.println (  capitansDaughter );
+    System.out.println (  fatharsAndSons );
+
+
+}
 }
